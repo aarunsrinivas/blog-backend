632543bc8b9f0f4c1c18066f38645d768c4079ce
Aarun Srinivas <aarunsrinivas@gmail.com>
2021-09-12 20:07:34 -0400

    added ssl

--- src/databases/sql.js
+++ src/databases/sql.js
@@ -1,7 +1,11 @@
 import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    ssl: true
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 })
 
 export default sequelize
